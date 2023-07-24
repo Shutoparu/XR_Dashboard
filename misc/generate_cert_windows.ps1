6596e70edae27ebc6ab345db4b30bde18f40e616
Shutoparu <thomaspeng0516@gmail.com>
2023-07-24 14:36:10 +0800

    minor syntax fix

--- misc/generate_cert_windows.ps1
+++ misc/generate_cert_windows.ps1
@@ -1,7 +1,7 @@
-$Host = (Get-Content "ip.txt" | Select-String -Pattern "Host" | ForEach-Object { $_.ToString().Split(' ')[1] })
+$myHost = (Get-Content "ip.txt" | Select-String -Pattern "Host" | ForEach-Object { $_.ToString().Split(' ')[1] })
 
 openssl genrsa -out "ssl_pem\cert-key.pem" 4096
-openssl req -new -sha256 -subj "/CN=$Host" -key "ssl_pem\cert-key.pem" -out "ssl_pem\cert.csr"
+openssl req -new -sha256 -subj "/CN=$myHost" -key "ssl_pem\cert-key.pem" -out "ssl_pem\cert.csr"
 openssl x509 -req -sha256 -days 3650 -in "ssl_pem\cert.csr" -CA "ssl_pem\ca.pem" -CAkey "ssl_pem\ca-key.pem" -out "ssl_pem\cert.pem" -extfile "ssl_pem\extfile.conf" -CAcreateserial
 Remove-Item "ssl_pem\cert.csr"
 Get-Content "ssl_pem\cert.pem", "ssl_pem\ca.pem" | Set-Content "ssl_pem\chain.pem"
